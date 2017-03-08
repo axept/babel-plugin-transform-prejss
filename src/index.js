10a2bbdf75d2e1146c3124cd649cf3f045968bd0
Grigory <grigory.leonenko@gmail.com>
2017-03-08 17:34:32 +0300

    Update example, hide src and example from npm, update parser dep, add parser from config and parser options.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import parser from 'prejss-postcss-parser'
 import extractExpressions from './extract-expressions'
 import restoreExpressions from './restore-expressions'
+
+let parser
 
 export default ({ types: t }) => {
   return {
     visitor: {
       TaggedTemplateExpression(p, { opts }) {
         const namespace = opts.namespace || 'prejss'
+        if (!parser) {
+          parser = require(opts.parser || 'prejss-postcss-parser')
+          if (typeof parser !== 'function') {
+            parser = parser.default
+          }
+        }
         const { tag } = p.node
 
         if (tag.name !== namespace) {
@@ -14,7 +21,7 @@ export default ({ types: t }) => {
         }
 
         const { rawStyle, variables } = extractExpressions(p.node.quasi)
-        const parsed = parser(rawStyle)
+        const parsed = parser(rawStyle, { config: opts.config })
         const restored = restoreExpressions(parsed, variables)
         p.replaceWith(restored)
       },
