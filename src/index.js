219663f3764280b90ab03711bb3001bfffaf8d67
Denis Izmaylov <izmaylov.dm@gmail.com>
2017-03-27 07:43:35 +0300

    Add removeImport option and turn it on by default

--- src/index.js
+++ src/index.js
@@ -6,16 +6,33 @@ let parser
 export default ({ types: t }) => {
   return {
     visitor: {
+      ImportDefaultSpecifier(p, { opts, file }) {
+        const removeImport = opts['removeImport'] || 'prejss'
+        if (removeImport === false) {
+          return
+        }
+        
+        const importRef = a.parentPath.node.source.value
+        if (importRef === removeImport) {
+          console.log(
+            `babel-plugin-transform-prejss: remove an import construction for ${importRef}` +
+            ` from ${file.opts.filename}"`
+          )
+          p.parentPath.remove()
+        }
+      },
+
       TaggedTemplateExpression(p, { opts }) {
-        const namespace = opts.namespace || 'prejss'
+        const namespace = opts['namespace'] || 'prejss'
+        
         if (!parser) {
           parser = require(opts.parser || 'prejss-postcss-parser')
           if (typeof parser !== 'function') {
             parser = parser.default
           }
         }
+        
         const { tag } = p.node
-
         if (tag.name !== namespace) {
           return
         }
